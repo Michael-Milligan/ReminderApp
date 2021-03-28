34cc3754bad093b1ea42f3c7863bd86a37d4c77e
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2021-03-28 11:41:12 +0300

    Began moving to MVVM

--- ReminderApp/MainWindow.xaml.cs
+++ ReminderApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ReminderApp.Models;
+using ReminderApp.VMs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -16,10 +18,11 @@ namespace ReminderApp
         public MainWindow()
         {
             InitializeComponent();
+            var model = new MainModel(this);
             Menu ThisMenu = (Content as DockPanel).Children[0] as Menu;
             Methods.MakeMenu(ref ThisMenu);
 
-            Thread AlertThread = new Thread(CheckIfItsTime)
+            Thread AlertThread = new Thread(MainModel.CheckIfItsTime)
             {
                 IsBackground = true
             };
@@ -28,72 +31,6 @@ namespace ReminderApp
 
         }
 
-        public static void Fn()
-        {
-            new Window().Show();
-        }
-
-        public static void CheckIfItsTime()
-        {
-            try
-            {
-                TasksContext Context = new TasksContext();
-                CurrentTask[] CurrentTasks = Context.CurrentTasks.ToArray();
-                IEnumerable<DateTime> Times = CurrentTasks.Select(item => item.Date_Time);
-
-                while (Times.Any() == true)
-                {
-                    DateTime Now = DateTime.Now;
-                    foreach (DateTime time in Times)
-                    {
-                        if (CompareTimes(Now, time))
-                        {
-                            CurrentTask temp = CurrentTasks.
-                                Where(item => CompareTimes(time, item.Date_Time)).First();
-                            Application.Current.Dispatcher.Invoke(() => Alert(temp));
-                            throw new FormatException();
-                        }
-                    }
-                }
-            }
-            catch (FormatException e) { }
-        }
-
-
-        public static void Alert(CurrentTask Task)
-        {
-            TimeAlert Alert = new TimeAlert(Task);
-            Application.Current.Windows[0].Content = Alert.Content;
-            Alert.MakeAlert();
-        }
-
-        private static bool CompareTimes(DateTime First, DateTime Second)
-        {
-            return First.Year == Second.Year &&
-                First.Month == Second.Month &&
-                First.Day == Second.Day &&
-                First.Hour == Second.Hour &&
-                First.Minute == Second.Minute;
-        }
-
-        private void ListOfCurrentTasks_Click(object sender, RoutedEventArgs e)
-        {
-            Content = new ListOfCurrentTasks().Content;
-        }
-
-        private void CompletedTasks_Click(object sender, RoutedEventArgs e)
-        {
-            Content = new CompletedTasksWindow().Content;
-        }
-
-        private void Invisible_ModeOn(object sender, RoutedEventArgs e)
-        {
-            Height = 0;
-            Width = 0;
-            WindowStyle = WindowStyle.None;
-            ShowInTaskbar = false;
-            ShowActivated = false;
-            WindowState = WindowState.Minimized;
-        }
+        
     }
 }
---
+++ ReminderApp/Models/MainModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ReminderApp.Models
+{
+    class MainModel
+    {
+        MainWindow window;
+        public MainModel(MainWindow window)
+        {
+            this.window = window;
+        }
+
+        public static void Fn()
+        {
+            new MainWindow().Show();
+        }
+
+        public static void CheckIfItsTime()
+        {
+            try
+            {
+                TasksContext Context = new TasksContext();
+                CurrentTask[] CurrentTasks = Context.CurrentTasks.ToArray();
+                IEnumerable<DateTime> Times = CurrentTasks.Select(item => item.Date_Time);
+
+                while (Times.Any() == true)
+                {
+                    DateTime Now = DateTime.Now;
+                    foreach (DateTime time in Times)
+                    {
+                        if (CompareTimes(Now, time))
+                        {
+                            CurrentTask temp = CurrentTasks.
+                                Where(item => CompareTimes(time, item.Date_Time)).First();
+                            Application.Current.Dispatcher.Invoke(() => Alert(temp));
+                            throw new FormatException();
+                        }
+                    }
+                }
+            }
+            catch (FormatException e) { }
+        }
+
+
+        public static void Alert(CurrentTask Task)
+        {
+            TimeAlert Alert = new TimeAlert(Task);
+            Application.Current.Windows[0].Content = Alert.Content;
+            Alert.MakeAlert();
+        }
+
+        private static bool CompareTimes(DateTime First, DateTime Second)
+        {
+            return First.Year == Second.Year &&
+                First.Month == Second.Month &&
+                First.Day == Second.Day &&
+                First.Hour == Second.Hour &&
+                First.Minute == Second.Minute;
+        }
+
+        private void ListOfCurrentTasks_Click(object sender, RoutedEventArgs e)
+        {
+            window.Content = new ListOfCurrentTasks().Content;
+        }
+
+        private void CompletedTasks_Click(object sender, RoutedEventArgs e)
+        {
+            window.Content = new CompletedTasksWindow().Content;
+        }
+
+        private void Invisible_ModeOn(object sender, RoutedEventArgs e)
+        {
+            window.Height = 0;
+            window.Width = 0;
+            window.WindowStyle = WindowStyle.None;
+            window.ShowInTaskbar = false;
+            window.ShowActivated = false;
+            window.WindowState = WindowState.Minimized;
+        }
+    }
+}
