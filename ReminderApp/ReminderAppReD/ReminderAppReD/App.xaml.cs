95c3ebd42ebc2ea03ff69f3813d9b0e00aabebb6
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2021-03-29 17:26:30 +0300

    Fixed the bug with initial language

--- ReminderApp/ReminderAppReD/ReminderAppReD/App.xaml.cs
+++ ReminderApp/ReminderAppReD/ReminderAppReD/App.xaml.cs
@@ -20,10 +20,7 @@ namespace ReminderAppReD
             MainViewModel.languages.Clear();
             MainViewModel.languages.Add(new CultureInfo("en-US"));
             MainViewModel.languages.Add(new CultureInfo("ru-RU"));
-        }
 
-        private void OnLoad(object sender, System.Windows.Navigation.NavigationEventArgs args)
-        {
             language = ReminderAppReD.Properties.Settings.Default.defaultLanguage;
         }
 
@@ -41,13 +38,10 @@ namespace ReminderAppReD
                 System.Threading.Thread.CurrentThread.CurrentUICulture = value;
 
                 ResourceDictionary dictionary = new ResourceDictionary();
-                _ = value.Name switch
-                {
-                    "en-US" => dictionary.Source = new Uri($@"Resources\lang.xaml", UriKind.Relative),
-                    _ => dictionary.Source = new Uri($@"Resources\lang.{value.Name}.xaml", UriKind.Relative)
-                };
+                dictionary.Source = new Uri($@"Resources\lang.{value.Name}.xaml", UriKind.Relative);
+                
 
-                ResourceDictionary oldDictionary = Application.Current.Resources.MergedDictionaries.First();
+                ResourceDictionary oldDictionary = Application.Current.Resources.MergedDictionaries.FirstOrDefault();
                 if (oldDictionary != null)
                 {
                     Application.Current.Resources.MergedDictionaries.Remove(oldDictionary);
