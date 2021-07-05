cbc2aba12ab587aeb979a9ba36688aae83e58a2b
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2021-07-05 13:13:23 +0300

    Fixed adding tasks

--- ReminderAppReD/VMs/AddcurrentTaskVM.cs
+++ ReminderAppReD/VMs/AddcurrentTaskVM.cs
@@ -10,12 +10,13 @@ namespace ReminderAppReD.VMs
     {
         public DelegateCommand SendCommand { get; set; } = new DelegateCommand(() => 
         { 
-            string TaskName = (Application.Current.Windows[1] as AddCurrentTaskView).NameTextBox.Text;
-            string TaskDate = (Application.Current.Windows[1] as AddCurrentTaskView).DateTextBox.Text;
+            string TaskName = (Application.Current.Windows[2] as AddCurrentTaskView).NameTextBox.Text;
+            string TaskDate = (Application.Current.Windows[2] as AddCurrentTaskView).DateTextBox.Text;
             try
             {
                 DateTime time = DateTime.Parse(TaskDate);
                 AddCurrentTaskWindowModel.AddNewTask(TaskName, time);
+                Application.Current.Windows[2].Close();
             }
             catch (FormatException)
             {
--- ReminderAppReD/Views/CurrentTasksTab.xaml.cs
+++ ReminderAppReD/Views/CurrentTasksTab.xaml.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Windows.Controls;
 
+
 namespace ReminderAppReD.Views
 {
     /// <summary>
