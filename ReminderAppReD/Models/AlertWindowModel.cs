d1947a745db4446a4629f532d78bcf70f55459ee
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2021-08-24 23:03:33 +0300

    Implemented PostponeDoneOk, need to test

--- ReminderAppReD/Models/AlertWindowModel.cs
+++ ReminderAppReD/Models/AlertWindowModel.cs
@@ -1,5 +1,10 @@
 ﻿using Prism.Mvvm;
 using ReminderAppReD.DB;
+using ReminderAppReD.Views;
+using ReminderAppReD.VMs;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace ReminderAppReD.Models
 {
@@ -11,19 +16,23 @@ namespace ReminderAppReD.Models
         public string postponingTime { get { return _postponingTime; } set { _postponingTime = value;
                 RaisePropertyChanged(nameof(_postponingTime));}}
 
-        public static void PostponeAlert(int minutes)
+        public static void PostponeAlert(int Id, int minutes)
         {
-
+            (((((Application.Current.Windows[0].Content as DockPanel).Children.Cast<UIElement>().ElementAt(1) as TabControl)
+                    .Items[0] as TabItem).Content as CurrentTasksTab).Resources["vm"] as CurrentTasksTabVM).model.PostponeTask(Id, minutes);
+            Ok();
         }
 
-        public static void Done()
+        public static void Done(int Id)
         {
-            //TasksContext context = new();
-            //CurrentTask task = context.CurrentTasks.Where(item => item.Id == taskId).First();
-            //context.CompletedTasks.Add(new() { CompletionDateTime = DateTime.Now, TaskId = task.Id });
-            //context.CurrentTasks.Remove(task);
-            //context.SaveChanges();
-            //TODO: Remake this
+            (((((Application.Current.Windows[0].Content as DockPanel).Children.Cast<UIElement>().ElementAt(1) as TabControl)
+                    .Items[0] as TabItem).Content as CurrentTasksTab).Resources["vm"] as CurrentTasksTabVM).model.MoveCurrentToCompleted(Id);
+            Ok();
+        }
+
+        public static void Ok()
+        {
+            (Application.Current.Windows.Cast<Window>().First(item => item.Title == "AlertWindow") as AlertWindow).Close();
         }
     }
 }
